041cb77adf110a1a223ce188aa42962cd5a31b7d
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-10-08 16:35:13 -0400

    one user route directs to correct route

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 export default class Home extends Component {
   state = {
@@ -8,12 +9,19 @@ export default class Home extends Component {
 
   componentDidMount = async () => {
     const response = await axios.get('/api/users')
-    console.log(response)
+    this.setState({users: response.data})
   }
   render() {
+    const userList = this.state.users.map((user, i) => {
+      return(
+        <div>
+          <Link to={`/users/${user._id}`}>name: {user.name}</Link>
+        </div>
+      )
+    })
     return (
       <div>
-        
+        {userList}
       </div>
     )
   }
