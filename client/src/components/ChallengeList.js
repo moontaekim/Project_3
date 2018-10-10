4bd33f40f171b0bb6c73ec4f6ff2c9be7c6320f0
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-10-10 14:42:01 -0400

    create new challenge form works

--- client/src/components/ChallengeList.js
+++ client/src/components/ChallengeList.js
@@ -27,6 +27,14 @@ export default class ChallengeList extends Component {
     this.setState({ newChallenge })
   }
 
+  handleSubmit = async (event) => {
+    event.preventDefault()
+    const response = await axios.post('/api/challenges', this.state.newChallenge)
+    const foodChallenges = [...this.state.foodChallenges]
+    foodChallenges.push(response.data)
+    this.setState({foodChallenges})
+  }
+
   render() {
     const foodChallengeList = this.state.foodChallenges.map((foodChallenge, i) => {
       return(
@@ -40,13 +48,13 @@ export default class ChallengeList extends Component {
       <div>
         {foodChallengeList}
         <form onSubmit={this.handleSubmit}>
-          <input type="text" name="name" value={this.state.newChallenge.name} onChange={this.handleChange}/>
-          <input type="text" name="location" value={this.state.newChallenge.location} onChange={this.handleChange}/>
-          <input type="text" name="address" value={this.state.newChallenge.address} onChange={this.handleChange}/>
-          <input type="text" name="price" value={this.state.newChallenge.price} onChange={this.handleChange}/>
-          <input type="text" name="difficulty" value={this.state.newChallenge.difficulty} onChange={this.handleChange}/>
-          <input type="text" name="state" value={this.state.newChallenge.state} onChange={this.handleChange}/>
-          <input type="text" name="time" value={this.state.newChallenge.time} onChange={this.handleChange}/>
+          <input type="text" placeholder= "name" name="name" value={this.state.newChallenge.name} onChange={this.handleChange}/>
+          <input type="text" placeholder= "location" name="location" value={this.state.newChallenge.location} onChange={this.handleChange}/>
+          <input type="text" placeholder= "address" name="address" value={this.state.newChallenge.address} onChange={this.handleChange}/>
+          <input type="text" placeholder= "price" name="price" value={this.state.newChallenge.price} onChange={this.handleChange}/>
+          <input type="text" placeholder= "difficulty" name="difficulty" value={this.state.newChallenge.difficulty} onChange={this.handleChange}/>
+          <input type="text" placeholder= "state" name="state" value={this.state.newChallenge.state} onChange={this.handleChange}/>
+          <input type="text" placeholder= "time" name="time" value={this.state.newChallenge.time} onChange={this.handleChange}/>
           <input type='submit' value='Create New Challenge'/>
         </form>
       </div>
