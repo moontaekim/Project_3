ad20dca46033b81b8da99f1f691781a4588db8b8
Moon-Tae Kim <moon-tae@Moon-Taes-MacBook-Air.local>
2018-10-10 16:40:20 -0400

    add new Challenge works correctly

--- client/src/components/ChallengeList/ChallengeList.js
+++ client/src/components/ChallengeList/ChallengeList.js
@@ -1,43 +1,46 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import NewChallengeForm from './NewChallengeForm';
 
 export default class ChallengeList extends Component {
   state = {
-    foodChallenges:[],
-    newChallenge:{
-      address:'',
-      difficulty:'',
-      location:'',
-      name:'',
-      price:'',
-      state:'',
-      time:''
+    foodChallenges: [],
+    newChallenge: {
+      address: '',
+      difficulty: '',
+      location: '',
+      name: '',
+      price: '',
+      state: '',
+      time: ''
     }
   }
 
   componentDidMount = async () => {
+    await this.getFoodChallenges()
+  }
+
+  getFoodChallenges = async () => {
     const response = await axios.get(`/api/challenges`)
-    this.setState({foodChallenges: response.data})
+    this.setState({ foodChallenges: response.data })
   }
 
   handleChange = (event) => {
-    const newChallenge = {...this.state.newChallenge}
+    const newChallenge = { ...this.state.newChallenge }
     newChallenge[event.target.name] = event.target.value
     this.setState({ newChallenge })
   }
 
   handleSubmit = async (event) => {
     event.preventDefault()
-    const response = await axios.post('/api/challenges', this.state.newChallenge)
-    const foodChallenges = [...this.state.foodChallenges]
-    foodChallenges.push(response.data)
-    this.setState({foodChallenges})
+    await axios.post('/api/challenges', this.state.newChallenge)
+    this.getFoodChallenges()
   }
 
   render() {
     const foodChallengeList = this.state.foodChallenges.map((foodChallenge, i) => {
-      return(
+      return (
         <div key={i}>
           <Link to={`/users/${this.props.match.params.userId}/challenges/${foodChallenge._id}`}>name: {foodChallenge.name}</Link>
         </div>
@@ -47,16 +50,11 @@ export default class ChallengeList extends Component {
     return (
       <div>
         {foodChallengeList}
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder= "name" name="name" value={this.state.newChallenge.name} onChange={this.handleChange}/>
-          <input type="text" placeholder= "location" name="location" value={this.state.newChallenge.location} onChange={this.handleChange}/>
-          <input type="text" placeholder= "address" name="address" value={this.state.newChallenge.address} onChange={this.handleChange}/>
-          <input type="text" placeholder= "price" name="price" value={this.state.newChallenge.price} onChange={this.handleChange}/>
-          <input type="text" placeholder= "difficulty" name="difficulty" value={this.state.newChallenge.difficulty} onChange={this.handleChange}/>
-          <input type="text" placeholder= "state" name="state" value={this.state.newChallenge.state} onChange={this.handleChange}/>
-          <input type="text" placeholder= "time" name="time" value={this.state.newChallenge.time} onChange={this.handleChange}/>
-          <input type='submit' value='Create New Challenge'/>
-        </form>
+        <NewChallengeForm
+          handleSubmit={this.handleSubmit}
+          newChallenge={this.state.newChallenge}
+          handleChange={this.handleChange}
+        />
       </div>
     )
   }
--- client/src/components/ChallengeList/NewChallengeForm.js
+++ client/src/components/ChallengeList/NewChallengeForm.js
@@ -4,14 +4,14 @@ export default class NewChallengeForm extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.handleSubmit}>
-          <input type="text" placeholder= "name" name="name" value={this.state.newChallenge.name} onChange={this.handleChange}/>
-          <input type="text" placeholder= "location" name="location" value={this.state.newChallenge.location} onChange={this.handleChange}/>
-          <input type="text" placeholder= "address" name="address" value={this.state.newChallenge.address} onChange={this.handleChange}/>
-          <input type="text" placeholder= "price" name="price" value={this.state.newChallenge.price} onChange={this.handleChange}/>
-          <input type="text" placeholder= "difficulty" name="difficulty" value={this.state.newChallenge.difficulty} onChange={this.handleChange}/>
-          <input type="text" placeholder= "state" name="state" value={this.state.newChallenge.state} onChange={this.handleChange}/>
-          <input type="text" placeholder= "time" name="time" value={this.state.newChallenge.time} onChange={this.handleChange}/>
+        <form onSubmit={this.props.handleSubmit}>
+          <input type="text" placeholder= "name" name="name" value={this.props.newChallenge.name} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "location" name="location" value={this.props.newChallenge.location} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "address" name="address" value={this.props.newChallenge.address} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "price" name="price" value={this.props.newChallenge.price} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "difficulty" name="difficulty" value={this.props.newChallenge.difficulty} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "state" name="state" value={this.props.newChallenge.state} onChange={this.props.handleChange}/>
+          <input type="text" placeholder= "time" name="time" value={this.props.newChallenge.time} onChange={this.props.handleChange}/>
           <input type='submit' value='Create New Challenge'/>
         </form>
       </div>
